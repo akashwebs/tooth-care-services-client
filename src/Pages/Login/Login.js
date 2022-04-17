e12dc933665fac0c5d54f11c6e90bf18cbb9e1f1
akashwebs <aksah.shil227@gmail.com>
2022-04-17 22:53:49 +0600

    added signout in header and added password login authentication

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../Firebase/firebase.init';
 import SocialLogin from '../../Shared/SocialLogin/SocialLogin';
 import './Login.css'
 
 const Login = () => {
-    const navgiate=useNavigate()
+    const navgiate=useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+      const handleLogin=event=>{
+          event.preventDefault();
+          const email=event.target.email.value;
+          const password=event.target.password.value;
+          signInWithEmailAndPassword(email, password);
+      }
+      
     return (
         <div className='container d-flex align-items-center ' style={{ height: '100vh ' }}>
             <div className='w-50 mx-auto p-4 form-section'>
                 <h2 className='mt-5 mb-3'>Sign Into Your Account</h2>
-                <Form>
+                <Form onSubmit={handleLogin}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control name='email' type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                            
                         </Form.Text>
@@ -21,7 +37,7 @@ const Login = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control name='password' type="password" placeholder="Password" />
                     </Form.Group>
                     
                     <button className='btn mb-2 btn-link text-decoration-none '>Forgot your password?</button>
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,10 +1,14 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import logo from '../../Assets/images/logo.png'
+import auth from '../../Firebase/firebase.init';
 import './Header.css'
 
 const Header = () => {
+    const [user, loading, error] = useAuthState(auth);
     return (
         <Navbar collapseOnSelect expand="lg" className='navbar' variant="light">
             <Container>
@@ -14,11 +18,11 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                       
-                        
+
+
                     </Nav>
                     <Nav>
-                       
+
                         <Nav.Link as={Link} to='home'>
                             Home
                         </Nav.Link>
@@ -28,9 +32,14 @@ const Header = () => {
                         <Nav.Link as={Link} to='about'>
                             About
                         </Nav.Link>
-                        <Nav.Link as={Link} to='login'>
-                            Login
-                        </Nav.Link>
+                        {
+                            user ?
+                                <button className='btn btn-link text-decoration-none' onClick={()=>signOut(auth)}>Log Out</button>
+                                :
+                                <Nav.Link as={Link} to='login'>
+                                    Login
+                                </Nav.Link>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
